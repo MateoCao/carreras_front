0007073bd39fab0c29750b50dedf28d07eb6c346
Mateo Cao <mateocao12@gmail.com>
2025-03-08 21:14:43 -0300

    first

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+// import LiveTiming from "./livetiming/Livetiming";
+import LiveTiming from "./livetiming/Livetimingg";
+
+export default function Home() {
+  return (
+    <div>
+      <LiveTiming />
+    </div>
+  );
+}
