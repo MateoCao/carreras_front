4d4e0d49aa48470e40c4dc406c5b0bdd88d233c5
Mateo Cao <mateocao12@gmail.com>
2025-03-11 16:26:01 -0300

    type fix

--- src/app/api/upload/route.ts
+++ src/app/api/upload/route.ts
@@ -1,34 +1,29 @@
-import { S3Client, PutObjectCommand } from "@aws-sdk/client-s3";
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
+import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
-if (
-    !process.env.AWS_REGION ||
-    !process.env.AWS_ACCESS_KEY_ID ||
-    !process.env.AWS_SECRET_ACCESS_KEY
-  ) {
-    throw new Error("Missing AWS environment variables");
-  }
-  
-  const s3 = new S3Client({
-    region: process.env.AWS_REGION,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    },
-  });
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+  },
+});
 
-export async function POST(req) {
+export async function POST(req: Request) {
   try {
     const formData = await req.formData();
-    const file = formData.get("file");
+    const file = formData.get('file');
 
-    if (!file) {
-      return NextResponse.json({ error: "No se proporcionó un archivo" }, { status: 400 });
+    if (!file || !(file instanceof File)) {
+      return NextResponse.json(
+        { error: 'No se proporcionó un archivo válido' },
+        { status: 400 },
+      );
     }
 
     const bytes = await file.arrayBuffer();
     const buffer = Buffer.from(bytes);
-    
+
     const uploadParams = {
       Bucket: process.env.AWS_S3_BUCKET,
       Key: file.name,
@@ -40,8 +35,15 @@ export async function POST(req) {
 
     const fileUrl = `https://${process.env.AWS_S3_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${file.name}`;
 
-    return NextResponse.json({ message: "Archivo subido exitosamente", url: fileUrl }, { status: 200 });
+    return NextResponse.json(
+      { message: 'Archivo subido exitosamente', url: fileUrl },
+      { status: 200 },
+    );
   } catch (error) {
-    return NextResponse.json({ error: error }, { status: 500 });
+    console.log(error)
+    return NextResponse.json(
+      { error: 'Error al subir el archivo' },
+      { status: 500 },
+    );
   }
 }
